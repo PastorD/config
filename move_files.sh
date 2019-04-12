43deaf9dff7c7273f4d04587481088790c772e1c
PastorD <dpastorm@caltech.edu>
2019-04-11 18:05:29 -0700

    Added bashrc, tmux.conf and few packages

---
+++ move_files.sh
@@ -0,0 +1,6 @@
+# use this file to add common definitions to the bashrc file. This file is loaded every time a terminal starts. 
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PARENT="$(dirname "$DIR")"
+cat "$PARENT/add_to_bashrc.txt" >> "$HOME/.bashrc"
+cp tmuxConf.conf ~/.tmux.conf
---
+++ packages.sh
@@ -0,0 +1,11 @@
+
+# Terminal Usage
+sudo apt-get install terminator tmux pytyon-tmuxp
+pip install tmuxp -U
+
+# IDE
+sudo apt-get install geany
+
+# Git
+sudo apt-get install kraken
+
